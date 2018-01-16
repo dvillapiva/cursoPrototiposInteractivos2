856214b9a635abc71eada529a0661acbb079ddee
dvillapiva <35501359+dvillapiva@users.noreply.github.com>
2018-01-16 13:22:05 -0600

    Update prácticaDirigida1.ino

--- prácticaDirigida1.ino
+++ prácticaDirigida1.ino
@@ -1,11 +1,18 @@
+//***************************************************
+//********Práctica dirigida.Sistema de alarma********
+//***************************************************
+
+
+
+//Variables que asocian los puertos del arduino.
 int pinRojo=3;
 int pinAzul =5;
 int piezo =6;
 int boton = 2;
 int estado =0;
- 
+
 void setup() {
-  // put your setup code here, to run once:
+  // Se le indica a los puertos anteriores el trabajo que harán.
 
     pinMode(pinRojo, OUTPUT);
     pinMode(pinAzul, OUTPUT);
@@ -15,26 +22,28 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    while (digitalRead(boton)==0)
+    while (digitalRead(boton)==0) // El botón está sin presionar
   {
+    //El led Rojo está encendido y el azul apagado en el tiempo determinado por el espera.
     digitalWrite(pinRojo, HIGH);
     digitalWrite(pinAzul, LOW);
     delay(500);
     
+    //El led Rojo está apagado y el azul encendido en el tiempo determinado por el espera..
     digitalWrite(pinRojo, LOW);
     digitalWrite(pinAzul, HIGH);
     delay(500);
-  
+
+    //Suena el piezo o parlante en dos notas diferentes.
     tone(piezo, 300,100);
     delay(100);
     tone(piezo, 450,50);
-    Serial.println(digitalRead(boton));
+    Serial.println(digitalRead(boton)); //Envía información al monitor
     delay(100);
   }
 
 
-      while (digitalRead(boton)==1)
+      while (digitalRead(boton)==1) //El botón está presionado.
   {
     digitalWrite(pinRojo, LOW);
     digitalWrite(pinAzul, LOW);
